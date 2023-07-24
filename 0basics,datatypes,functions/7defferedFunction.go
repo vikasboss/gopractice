ff443d6669791adca2033ab7dde0f533175e89a9
vikasboss6855 <vikassharma.0407@gmail.com>
2023-07-24 11:27:39 +0530

    aded code for variable functions

---
+++ 0basics,datatypes,functions/7defferedFunction.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	PrintHello()
+}
+
+func PrintWorld() {
+	fmt.Print(" World")
+}
+
+// Defer is a spezial statement that schedules functions to be executed after the function completes
+
+func PrintHello() {
+	defer PrintWorld()
+	defer fmt.Print("Hello")
+
+}
