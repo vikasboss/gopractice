c87e572b3abda286d61ca70c7b0b7b8fc27578e0
vikasboss6855 <vikassharma.0407@gmail.com>
2023-07-24 10:51:55 +0530

    added more context

--- basics/2ConvertingDatatypes.go
+++ basics/2ConvertingDatatypes.go
@@ -1,3 +1,25 @@
+/*
+uint8       unsigned  8-bit integers (0 to 255)
+uint16      unsigned 16-bit integers (0 to 65535)
+uint32      unsigned 32-bit integers (0 to 4294967295)
+uint64      unsigned 64-bit integers (0 to 18446744073709551615)
+int8        signed  8-bit integers (-128 to 127)
+int16       signed 16-bit integers (-32768 to 32767)
+int32       signed 32-bit integers (-2147483648 to 2147483647)
+int64       signed 64-bit integers (-9223372036854775808 to 9223372036854775807)
+
+float32     IEEE-754 32-bit floating-point numbers
+float64     IEEE-754 64-bit floating-point numbers
+complex64   complex numbers with float32 real and imaginary parts
+complex128  complex numbers with float64 real and imaginary parts
+
+byte        alias for uint8
+rune        alias for int32
+
+uint     unsigned, either 32 or 64 bits
+int      signed, either 32 or 64 bits
+uintptr  unsigned integer large enough to store the uninterpreted bits of a pointer value
+*/
 package main
 
 import (
@@ -13,6 +35,17 @@ Float to String
 Converting float to int
 */
 func main() {
+	// Declaring multiple variables
+	firstName, lastName := "FirstName", "LastName"
+
+	fmt.Println(firstName + lastName)
+
+	// Variable Declaration Block
+	var (
+		name = "Donald Duck"
+		age  = 50
+	)
+
 	aString := "abcd"
 	fmt.Println(aString)
 	//reflect is used to check for types in golang
